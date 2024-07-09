ddd76a729aa500e09fc2493f4b38b414e61438eb
Michael Pham <61564344+Mikefly123@users.noreply.github.com>
2024-07-09 16:10:50 -0700

    Added Latest Firmware
    Newly compiled firmware for the FC board added to the repo!

--- Firmware/pysquared_rp2040_flight_controller_v1a/mpconfigboard.h
+++ Firmware/pysquared_rp2040_flight_controller_v1a/mpconfigboard.h
@@ -1,25 +1,22 @@
-#define MICROPY_HW_BOARD_NAME "Battery Board"
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2021 Scott Shawcroft for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#define MICROPY_HW_BOARD_NAME "Adafruit Feather RP2040"
 #define MICROPY_HW_MCU_NAME "rp2040"
-// External flash MR2xH40 MRAM
-#define EXTERNAL_FLASH_QSPI_SINGLE
-#define EXTERNAL_FLASH_NO_JEDEC
 
+#define MICROPY_HW_NEOPIXEL (&pin_GPIO16)
 
-#define BOARD_FLASH_SIZE (FLASH_SIZE - 0x4000 - CIRCUITPY_INTERNAL_NVM_SIZE)
-
-#define CIRCUITPY_DRIVE_LABEL "FLIGHT CONTROLLER BOARD V1a"
-#define CIRCUITPY_BOOT_COUNTER 1
-
-#define BOARD_HAS_CRYSTAL 1
-
-#define MICROPY_HW_NEOPIXEL (&pin_GPIO24)
-
-#define DEFAULT_I2C_BUS_SDA (&pin_GPIO2)
 #define DEFAULT_I2C_BUS_SCL (&pin_GPIO3)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO2)
 
-#define DEFAULT_UART_BUS_TX (&pin_GPIO0)
+#define DEFAULT_SPI_BUS_SCK (&pin_GPIO10)
+#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO8)
+#define DEFAULT_SPI_BUS_MISO (&pin_GPIO11)
+
 #define DEFAULT_UART_BUS_RX (&pin_GPIO1)
-
-#define DEFAULT_SPI_BUS_SCK (&pin_GPIO18)
-#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO19)
-#define DEFAULT_SPI_BUS_MISO (&pin_GPIO16)
+#define DEFAULT_UART_BUS_TX (&pin_GPIO0)
