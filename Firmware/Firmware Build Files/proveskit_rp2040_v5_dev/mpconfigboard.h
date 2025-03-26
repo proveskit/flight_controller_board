69e1d17430765f63f1789c868661713a2bf8908d
Michael Pham <61564344+Mikefly123@users.noreply.github.com>
2025-03-26 12:34:00 -0700

    Added V5 Dev Board Firmware Build Files

--- Firmware/Firmware Build Files/proveskit_rp2040_v5_dev/mpconfigboard.h
+++ Firmware/Firmware Build Files/proveskit_rp2040_v5_dev/mpconfigboard.h
@@ -1,21 +1,17 @@
 // This file is part of the CircuitPython project: https://circuitpython.org
 //
-// SPDX-FileCopyrightText: Copyright (c) 2021 Scott Shawcroft for Adafruit Industries
+// SPDX-FileCopyrightText: Copyright (c) 2024 Adafruit Industries LLC
 //
 // SPDX-License-Identifier: MIT
 
 #pragma once
 
-#define MICROPY_HW_BOARD_NAME "PySquaredFCv4"
+#define MICROPY_HW_BOARD_NAME "PROVES Kit v4"
 #define MICROPY_HW_MCU_NAME "rp2040"
 
 #define MICROPY_HW_NEOPIXEL (&pin_GPIO24)
 
-#define CIRCUITPY_INTERNAL_NVM_SIZE (4 * 1024)
-#define CIRCUITPY_INTERNAL_FLASH_FILESYSTEM_SIZE (48 * 1024)
-#define BOARD_FLASH_SIZE (FLASH_SIZE - 0x4000 - CIRCUITPY_INTERNAL_NVM_SIZE)
-
-#define CIRCUITPY_DRIVE_LABEL "PYSQUARED"
+#define CIRCUITPY_DRIVE_LABEL "PROVESKIT"
 
 #define DEFAULT_I2C_BUS_SCL (&pin_GPIO3)
 #define DEFAULT_I2C_BUS_SDA (&pin_GPIO2)
